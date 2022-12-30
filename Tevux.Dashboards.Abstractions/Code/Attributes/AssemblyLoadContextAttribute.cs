89072deb7b3528f29e7642ade4c53ebd48709523
devbotas <simon@devbot.lt>
2022-12-30 13:44:39 +0200

    Changing AssemblyLoadContextAttribute.

--- Tevux.Dashboards.Abstractions/Code/Attributes/AssemblyLoadContextAttribute.cs
+++ Tevux.Dashboards.Abstractions/Code/Attributes/AssemblyLoadContextAttribute.cs
@@ -3,12 +3,23 @@
 
 [AttributeUsage(AttributeTargets.Assembly)]
 public class AssemblyLoadContextAttribute : Attribute {
+    [Obsolete("ContextName and ContextFriendlyName properties are obsolete.")]
     public AssemblyLoadContextAttribute(string contextName, string friendlyName) {
         ContextName = contextName;
         ContextFriendlyName = friendlyName;
     }
+
+    public AssemblyLoadContextAttribute(string friendlyName) {
+        FriendlyName = friendlyName;
+    }
+
     public AssemblyLoadContextAttribute() { }
 
-    public string ContextName { get; set; } = "Default";
+    [Obsolete("Use FriendlyName instead.")]
     public string ContextFriendlyName { get; set; } = "";
+
+    [Obsolete("Multi-library contexts aren't used anymore.")]
+    public string ContextName { get; set; } = "Default";
+
+    public string FriendlyName { get; set; } = "";
 }
