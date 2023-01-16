5a8d6b017e9304251a6bd5f4e0f45a9c5b0c64c7
devbotas <simon@devbot.lt>
2023-01-16 14:53:56 +0200

    Kinda replacing IEridanusControl by IErrorMessageProvider.

--- Tevux.Dashboards.Abstractions/Code/IEridanusControl.cs
+++ Tevux.Dashboards.Abstractions/Code/IEridanusControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 
 namespace Tevux.Dashboards.Abstractions;
+[Obsolete()]
 public interface IEridanusControl : IDisposable, INotifyPropertyChanged {
     string ErrorMessage { get; }
 }
---
+++ Tevux.Dashboards.Abstractions/Code/IErrorMessageProvider.cs
@@ -0,0 +1,4 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+public interface IErrorMessageProvider {
+    string ErrorMessage { get; }
+}
