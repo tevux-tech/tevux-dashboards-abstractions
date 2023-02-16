520e823a1f01336637b481faae8a1b192e3e3ca0
devbotas <simon@devbot.lt>
2023-02-16 12:21:11 +0200

    Ditching IEridanusControl.

--- Tevux.Dashboards.Abstractions/Code/IEridanusControl.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Tevux.Dashboards.Abstractions;
-[Obsolete()]
-public interface IEridanusControl : IDisposable, INotifyPropertyChanged {
-    string ErrorMessage { get; }
-}
