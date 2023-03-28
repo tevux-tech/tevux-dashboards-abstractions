e3a8719a2fdff58202ff9cb478be013dc0299ea5
devbotas <simon@devbot.lt>
2023-03-28 14:41:13 +0300

    Iterating on ICache.

---
+++ Tevux.Dashboards.Abstractions/Code/ICache.cs
@@ -0,0 +1,17 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+
+public interface ICache {
+    public bool TryRead(object owner, string propertyName, out object? value);
+    void Write(object owner, string propertyName, object value);
+}
+
+public class EmptyCache : ICache {
+    public bool TryRead(object owner, string propertyName, out object? value) {
+        value = null;
+        return false;
+    }
+
+    public void Write(object owner, string propertyName, object value) {
+
+    }
+}
