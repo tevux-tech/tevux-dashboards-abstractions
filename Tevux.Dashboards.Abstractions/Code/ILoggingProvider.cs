7864b82c22ecca681c57e00316270f21d70032cf
devbotas <simon@devbot.lt>
2023-05-16 15:21:49 +0300

    Adding ILoggingProvider.

---
+++ Tevux.Dashboards.Abstractions/Code/ILoggingProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+public interface ILoggingProvider {
+    public void Error(string message);
+    public void Info(string message);
+    public void Warn(string message);
+}
+
+public class EmptyLoggingProvider : ILoggingProvider {
+    public void Error(string message) { }
+    public void Info(string message) { }
+    public void Warn(string message) { }
+}
