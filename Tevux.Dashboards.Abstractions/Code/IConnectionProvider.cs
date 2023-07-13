9f27e81350e92678f546682de887fa17f61d0798
devbotas <simon@devbot.lt>
2023-07-13 16:30:44 +0300

    Releasing 0.71.0.

--- Tevux.Dashboards.Abstractions/Code/IConnectionProvider.cs
+++ Tevux.Dashboards.Abstractions/Code/IConnectionProvider.cs
@@ -2,5 +2,9 @@
 
 public interface IConnectionProvider {
     IConnection Connection { get; }
+
+    [Obsolete("Replaced by ConnectionOptionsControl")]
     public object ConnectionGuiControl { get; }
+
+    public Type ConnectionOptionsControl { get; }
 }
