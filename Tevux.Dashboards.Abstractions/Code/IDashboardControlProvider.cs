a67627c6d5c3e0751d04573aede10fab70be56c4
devbotas <simon@devbot.lt>
2023-08-01 13:47:34 +0300

    Documenting IDashboardControlProvider.

--- Tevux.Dashboards.Abstractions/Code/IDashboardControlProvider.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Tevux.Dashboards.Abstractions;
-
-public interface IDashboardControlProvider {
-    public List<Type> DashboardControls { get; }
-}
---
+++ Tevux.Dashboards.Abstractions/Code/Interfaces/IDashboardControlProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+
+/// <summary>
+/// Provides graphical dashboard controls. Usually it is the same class that implements <see cref="ILibrary"/>.
+/// </summary>
+public interface IDashboardControlProvider {
+    /// <summary>
+    /// List of graphical WPF controls that could be used in a dashboard. Although not specifically resctricted here, type must inherit from <c>FrameworkElement</c>, or it will not be recognised otherwise. />
+    /// </summary>
+    public List<Type> DashboardControls { get; }
+}
