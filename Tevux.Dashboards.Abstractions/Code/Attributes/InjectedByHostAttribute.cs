30520e285991f55295c95a26d62c0853e43c1c1d
devbotas <simon@devbot.lt>
2023-08-02 08:09:15 +0300

    Releasing 0.71.0-preview.10.

--- Tevux.Dashboards.Abstractions/Code/Attributes/InjectedByHostAttribute.cs
+++ Tevux.Dashboards.Abstractions/Code/Attributes/InjectedByHostAttribute.cs
@@ -1,5 +1,8 @@
 ﻿
 namespace Tevux.Dashboards.Abstractions;
 
+/// <summary>
+/// Properties marked with this attribute will get their values injected by host application at runtime.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property)]
 public class InjectedByHostAttribute : Attribute { }
