c0629d842d4a661fae3053441a518201817fc597
devbotas <simon@devbot.lt>
2023-08-21 13:29:21 +0300

    LibraryLoadingComplete and DashboardLoadingComplete are now different messages.

---
+++ Tevux.Dashboards.Abstractions/Code/Messaging/DashboardLoadingCompleteMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+
+/// <summary>
+/// Indication that the loading of the dashboard controls is complete.
+/// </summary>
+public class DashboardLoadingCompleteMessage : GenericMessage { }
--- Tevux.Dashboards.Abstractions/Code/Messaging/LibraryLoadingCompleteMessage.cs
+++ Tevux.Dashboards.Abstractions/Code/Messaging/LibraryLoadingCompleteMessage.cs
@@ -3,4 +3,4 @@
 /// <summary>
 /// Indication that the loading of dashboard libraries and controls is complete.
 /// </summary>
-public class LoadingCompleteMessage : GenericMessage { }
+public class LibraryLoadingCompleteMessage : GenericMessage { }
