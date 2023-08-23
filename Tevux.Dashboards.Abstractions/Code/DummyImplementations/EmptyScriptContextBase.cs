5160b2ad0642856bb3c53030c6807ea713251ed9
devbotas <simon@devbot.lt>
2023-08-23 13:19:39 +0300

    ScriptContextBase postponed for better future.

--- Tevux.Dashboards.Abstractions/Code/DummyImplementations/EmptyScriptContextBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tevux.Dashboards.Abstractions;
-
-/// <summary>
-/// An empty implementation of <see cref="ScriptContextBase"/> to use instead of <c>null</c>.
-/// </summary>
-public sealed class EmptyScriptContextBase : ScriptContextBase {
-    public override ISharedLibraryMessagingProvider Messenger { get; } = new EmptySharedLibraryMessagingProvider();
-}
--- Tevux.Dashboards.Abstractions/Code/ScriptContextBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tevux.Dashboards.Abstractions;
-
-public abstract class ScriptContextBase {
-    public bool IsCancellationRequested { get; set; }
-    public abstract ISharedLibraryMessagingProvider Messenger { get; }
-    public void WriteLine(string text) {
-        Messenger.Send("editor-debug-output", new SetValueMessage(text));
-    }
-}
