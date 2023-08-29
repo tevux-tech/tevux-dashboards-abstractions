df00ca3d0f671479638b6341104d2a6ed8bddddb
devbotas <simon@devbot.lt>
2023-08-29 10:48:59 +0300

    Adding Alignment enum.

---
+++ Tevux.Dashboards.Abstractions/Code/Controls/Alignment.cs
@@ -0,0 +1,12 @@
+﻿namespace Tevux.Dashboards.Abstractions;
+
+/// <summary>
+/// Alignment of one control inside another one. Usually used for text labels.
+/// </summary>
+public enum Alignment {
+    Left,
+    Top,
+    Right,
+    Bottom,
+    Middle
+}
