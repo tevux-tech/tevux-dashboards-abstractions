427a33b621696f4732a92e90be0980300fb569ae
devbotas <simon@devbot.lt>
2024-01-10 14:16:56 +0200

    Defining generics more strictly.

--- Tevux.Dashboards.Abstractions/Code/DummyImplementations/EmptySharedLibraryMessagingProvider.cs
+++ Tevux.Dashboards.Abstractions/Code/DummyImplementations/EmptySharedLibraryMessagingProvider.cs
@@ -8,43 +8,33 @@ public class EmptySharedLibraryMessagingProvider : ISharedLibraryMessagingProvid
         value = new();
     }
 
-    public void Register<TMessage>(object recipient, string token, Action<TMessage> action) {
-
+    public void Register<TMessage>(object recipient, string token, Action<TMessage> action) where TMessage : class {
     }
 
-    public void Register<TMessage>(object recipient, Action<TMessage> action) {
-
+    public void Register<TMessage>(object recipient, Action<TMessage> action) where TMessage : class {
     }
 
     public void Register(object recipient, string token, Action<object> action) {
-
     }
 
-    public void Send<TMessage>(TMessage message) {
-
+    public void Send<TMessage>(TMessage message) where TMessage : class {
     }
 
-    public void Send<TMessage>(string token, TMessage message) {
-
+    public void Send<TMessage>(string token, TMessage message) where TMessage : class {
     }
 
     public void SetValue(string token, object value) {
-
     }
 
-    public void Unregister<TMessage>(object recipient, string token, Action<TMessage> action) {
-
+    public void Unregister<TMessage>(object recipient, string token, Action<TMessage> action) where TMessage : class {
     }
 
-    public void Unregister<TMessage>(object recipient, Action<TMessage> action) {
-
+    public void Unregister<TMessage>(object recipient, Action<TMessage> action) where TMessage : class {
     }
 
     public void Unregister(object recipient, string token, Action<object> action) {
-
     }
 
     public void Unregister(object recipient) {
-
     }
 }
--- Tevux.Dashboards.Abstractions/Code/Messaging/ISharedLibraryMessagingProvider.cs
+++ Tevux.Dashboards.Abstractions/Code/Messaging/ISharedLibraryMessagingProvider.cs
@@ -17,24 +17,24 @@ public interface ISharedLibraryMessagingProvider {
     /// <summary>
     /// Broadcasts a message containing structured data./>
     /// </summary>
-    public void Send<TMessage>(TMessage message);
+    public void Send<TMessage>(TMessage message) where TMessage : class;
 
     /// <summary>
     /// Sends structured data to a specific listener, identified by a token.
     /// </summary>
-    public void Send<TMessage>(string token, TMessage message);
+    public void Send<TMessage>(string token, TMessage message) where TMessage : class;
 
     /// <summary>
     /// Registers to listen to structured messages, identified by a message type and the token.
     /// </summary>
     /// <param name="recipient">Usually 'this'.</param>
-    public void Register<TMessage>(object recipient, string token, Action<TMessage> action);
+    public void Register<TMessage>(object recipient, string token, Action<TMessage> action) where TMessage : class;
 
     /// <summary>
     /// Registers to listen to structured messages, identified by a message type only.
     /// </summary>
     /// <param name="recipient">Usually 'this'.</param>
-    public void Register<TMessage>(object recipient, Action<TMessage> action);
+    public void Register<TMessage>(object recipient, Action<TMessage> action) where TMessage : class;
 
     /// <summary>
     /// Registers to listen to unstructured messages, identified by the token only.
@@ -46,13 +46,13 @@ public interface ISharedLibraryMessagingProvider {
     /// Unregisters from structured messages, identified by a message type and the token.
     /// </summary>
     /// <param name="recipient">Usually 'this'.</param>
-    public void Unregister<TMessage>(object recipient, string token, Action<TMessage> action);
+    public void Unregister<TMessage>(object recipient, string token, Action<TMessage> action) where TMessage : class;
 
     /// <summary>
     /// Unregisters from structured messages, identified by a message type only.
     /// </summary>
     /// <param name="recipient">Usually 'this'.</param>
-    public void Unregister<TMessage>(object recipient, Action<TMessage> action);
+    public void Unregister<TMessage>(object recipient, Action<TMessage> action) where TMessage : class;
 
     /// <summary>
     /// Unregisters from structured messages, identified by token only.
